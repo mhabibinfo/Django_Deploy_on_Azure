524078ced759b39b08cc17f1a40e66b1aaa844d9
mhabibinfo <muhammad123@gmail.com>
2024-01-31 02:33:29 +0500

    Add stagging file 1

--- azure_project/wsgi.py
+++ azure_project/wsgi.py
@@ -2,13 +2,13 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 #settings_module = 'azure_project.deployment' if 'WEBSITE_HOSTNAME' in os.environ else 'azure_project.settings'
-if 'allurepro-staging.azurewebsites.net' in os.environ:
+if 'WEBSITE_HOSTNAME=allurepro-staging.azurewebsites.net' in os.environ:
     settings_module = 'azure_project.staging'
-elif 'allurepro.azurewebsites.net' in os.environ:
+elif 'WEBSITE_HOSTNAME=allurepro.azurewebsites.net' in os.environ:
     settings_module = 'azure_project.deployment'
 else:
     settings_module = 'azure_project.settings'
-    
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
 
 application = get_wsgi_application()
